97ae24c718640fe097ab963ad5c808fc8416dbd7
Naincy31 <iamnaincyrathore@gmail.com>
2024-10-08 12:45:59 +0530

    Created nested structure

--- first episode/App.js
+++ first episode/App.js
@@ -1,10 +1,9 @@
 const heading = React.createElement("h1", {id: "heading"}, "Hello World from React")
 
-console.log(heading);
+const para = React.createElement("p", {}, "I'm the sibling of the h1 tag")
+
+const parent = React.createElement("div", {id: "parent"}, [heading, para])
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 
-console.log(root);
-
-
-root.render(heading)
+root.render(parent)
