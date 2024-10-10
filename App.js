75f07d9a4c71b8448a0f67592c85554bcb7dccfc
Naincy31 <iamnaincyrathore@gmail.com>
2024-10-10 11:41:08 +0530

    Components and Component Composition

--- App.js
+++ App.js
@@ -1,12 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client"
 
-const heading = React.createElement("h1", {id: "heading"}, "Hello World from React")
+const Heading = () => <h1 id="heading">Hello World from React</h1>
 
 const jsxHeading = <h1 className="head">Hello React using JSX</h1>
 
-const parent = React.createElement("div", {id: "container"}, [heading, jsxHeading])
-  
+//React Component
+const Parent = () => (<div>
+    <Heading/>
+    {jsxHeading}
+</div>
+)
+
 const root = ReactDOM.createRoot(document.getElementById("root"))
 
-root.render(parent)
+root.render(<Parent/>)
